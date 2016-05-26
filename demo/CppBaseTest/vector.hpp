ad248310844d96737bee438b1b2c5434468eda52
fengbingchun <fengbingchun@163.com>
2016-05-26 22:03:41 +0800

    Add std::vector test code

---
+++ demo/CppBaseTest/vector.hpp
@@ -0,0 +1,9 @@
+#ifndef FBC_MESSY_TEST_VECTOR_HPP
+#define FBC_MESSY_TEST_VECTOR_HPP
+
+int test_vector_init();
+int test_vector_access();
+int test_vector_operation();
+int test_vector_two_dimension();
+
+#endif // FBC_MESSY_TEST_VECTOR_HPP
