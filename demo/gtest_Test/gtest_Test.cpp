f90a8f24dddf0052cf54b6487dd1586ead3d8161
fengbingchun <fengbingchun@163.com>
2018-08-03 14:26:03 +0800

    Modify build.sh script in gtest_Test project

--- demo/gtest_Test/gtest_Test.cpp
+++ demo/gtest_Test/gtest_Test.cpp
@@ -13,7 +13,7 @@ int main(int argc, char* argv[])
 		"--gtest_filter=*", // 指定需要运行的tests
 		//"--gtest_print_time=0", // don't print the elapsed time of each test
 #ifdef __linux__
-		"--gtest_output=xml:./build/info.xml"
+		"--gtest_output=xml:testdata/info.xml" // 结果输出到指定的xml文件
 #else
 		"--gtest_output=xml:E:/GitCode/Messy_Test/testdata/info.xml" // 结果输出到指定的xml文件
 #endif
